f004f7e8bd2b0ba9ae3244502a8f99567041205c
Renzo Bauen <info@conpassione.ch>
2024-03-19 19:46:28 +0100

    Frontend Rendering implemented

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,4 +5,4 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3') or die();
 
-ExtensionManagementUtility::addUserTSConfig('@import "EXT:cpdevsite/Configuration/TsConfig/User/user.tsconfig"');
+ExtensionManagementUtility::addUserTSConfig('@import "EXT:cpdevsite/Configuration/user.tsconfig"');
