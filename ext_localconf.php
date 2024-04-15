b0378208968dc484c535d2bb3ff433a94c79aa84
Renzo Bauen <info@conpassione.ch>
2024-04-15 16:09:07 +0200

    Config CK-Editor: custom toolbar

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,6 +8,6 @@ defined('TYPO3') or die();
 ExtensionManagementUtility::addUserTSConfig('@import "EXT:cpdevsite/Configuration/TsConfig/User/user.tsconfig"');
 
 // CK-Editor Konfiguration laden
-//if (empty($GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['cpdevsite'])) {
+if (empty($GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['cpdevsite'])) {
     $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['cpdevsite'] = 'EXT:cpdevsite/Configuration/RTE/RTEsettings.yaml';
-//}
+}
