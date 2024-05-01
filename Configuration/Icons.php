53215fc665a9c497fc32a68f06cfb5b2f5c56c59
Renzo Bauen <info@conpassione.ch>
2024-05-01 14:05:15 +0200

    Erste Ausgabe mit Fluid

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -5,55 +5,39 @@ use TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider;
 use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
 
 return [
-    // Icon identifier
     'tx-conpassione-content-beside-text-img-left' => [
-        // Icon provider class
         'provider' => SvgIconProvider::class,
-        // The source SVG for the SvgIconProvider
         'source' => 'EXT:cpdevsite/Resources/Public/Icons/content-beside-text-img-left.svg',
     ],
     'tx-conpassione-content-beside-text-img-left-middle' => [
-        // Icon provider class
         'provider' => SvgIconProvider::class,
-        // The source SVG for the SvgIconProvider
         'source' => 'EXT:cpdevsite/Resources/Public/Icons/content-beside-text-img-left-middle.svg',
     ],
     'tx-conpassione-content-beside-text-img-left-bottom' => [
-        // Icon provider class
         'provider' => SvgIconProvider::class,
-        // The source SVG for the SvgIconProvider
         'source' => 'EXT:cpdevsite/Resources/Public/Icons/content-beside-text-img-left-bottom.svg',
     ],
-
     'tx-conpassione-content-beside-text-img-right' => [
-        // Icon provider class
         'provider' => SvgIconProvider::class,
-        // The source SVG for the SvgIconProvider
         'source' => 'EXT:cpdevsite/Resources/Public/Icons/content-beside-text-img-right.svg',
     ],
     'tx-conpassione-content-beside-text-img-right-middle' => [
-        // Icon provider class
         'provider' => SvgIconProvider::class,
-        // The source SVG for the SvgIconProvider
         'source' => 'EXT:cpdevsite/Resources/Public/Icons/content-beside-text-img-right-middle.svg',
     ],
     'tx-conpassione-content-beside-text-img-right-bottom' => [
-        // Icon provider class
         'provider' => SvgIconProvider::class,
-        // The source SVG for the SvgIconProvider
         'source' => 'EXT:cpdevsite/Resources/Public/Icons/content-beside-text-img-right-bottom.svg',
     ],
+/*  Im BackendLayout wird das Icon direkt vom File geladen, damit das Icon grösser dargestellt werden kann
     'tx-conpassione-template-simple-site' => [
-        // Icon provider class
         'provider' => SvgIconProvider::class,
-        // The source SVG for the SvgIconProvider
-        'source' => 'EXT:cpdevsite/Resources/Public/Icons/template-simple-site.svg',
-    ],
+        'source' => 'EXT:cpdevsite/Resources/Public/Icons/template-simple-page.svg',
+    ],*/
 
 
 /*    'tx-myext-bitmapicon' => [
         'provider' => BitmapIconProvider::class,
-        // The source bitmap file
         'source' => 'EXT:my_extension/Resources/Public/Icons/mybitmap.png',
         // All icon providers provide the possibility to register an icon that spins
         'spinning' => true,
