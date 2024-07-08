ca7a7b963acb459a842123ab3cf290732c193994
Renzo Bauen <info@conpassione.ch>
2024-07-08 15:37:41 +0200

    Back from Hollidays

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,9 +5,15 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3') or die();
 
-ExtensionManagementUtility::addUserTSConfig('@import "EXT:cpdevsite/Configuration/TsConfig/User/user.tsconfig"');
+ExtensionManagementUtility::addUserTSConfig('@import "EXT:' . getenv('TYPO3_EXT_IDENTIFIER') . '/Configuration/TsConfig/User/user.tsconfig"');
 
 // CK-Editor Konfiguration laden
-if (empty($GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['cpdevsite'])) {
-    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['cpdevsite'] = 'EXT:cpdevsite/Configuration/RTE/RTEsettings.yaml';
+if (empty($GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets'][getenv('TYPO3_EXT_IDENTIFIER')])) {
+    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets'][getenv('TYPO3_EXT_IDENTIFIER')] = 'EXT:' . getenv('TYPO3_EXT_IDENTIFIER') . '/Configuration/RTE/RTEsettings.yaml';
 }
+
+// Add custom translations overriding default labels
+// $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:frontend/Resources/Private/Language/locallang_tca.xlf'][] = 'EXT:' . getenv('TYPO3_EXT_IDENTIFIER') .
+// '/Resources/Private/Language/custom.xlf';
+// $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['de']['EXT:frontend/Resources/Private/Language/locallang_tca.xlf'][] ='EXT:' . getenv('TYPO3_EXT_IDENTIFIER') .
+// '/Resources/Private/Language/de.custom.xlf';
