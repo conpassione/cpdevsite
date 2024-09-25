1dd6d77a803a83dce1e83183db12aa7c290922aa
Renzo Bauen <info@conpassione.ch>
2024-09-25 15:32:55 +0200

    Siteset introduced

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -6,7 +6,7 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 defined('TYPO3') or die();
 
 // Typoscript konfigurieren
-ExtensionManagementUtility::addStaticFile(
+/*ExtensionManagementUtility::addStaticFile(
     getenv('TYPO3_EXT_IDENTIFIER'),
     'Configuration/TypoScript',
-    'Conpassione Sitepackage DEV');
+    'Conpassione Sitepackage DEV');*/
