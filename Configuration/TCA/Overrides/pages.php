6701607f3c3b105f4e7f99f5e96c0b052d25c3fe
Renzo Bauen <info@conpassione.ch>
2024-10-08 12:56:05 +0200

    TCA pages angepasst, Update auf TYPO3 13.3.1

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 defined('TYPO3') or die();
 
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 call_user_func(static function(): void {
     // clear the default items for "layout" field to allow for consistent adding of additional items with addItems in
@@ -10,5 +11,32 @@ call_user_func(static function(): void {
     $GLOBALS['TCA']['pages']['columns']['layout']['config']['items'] = [];
     unset($GLOBALS['TCA']['pages']['columns']['layout']['config']['default']);
 
+    $doktypes = '36650001,36650002';
 
+    ExtensionManagementUtility::addToAllTCAtypes(
+        'pages',
+        'abstract',
+        $doktypes,
+        'before:keywords'
+    );
+
+    ExtensionManagementUtility::addFieldsToPalette(
+        'pages',
+        'editorial',
+        'author, author_email, lastUpdated'
+    );
+
+    ExtensionManagementUtility::addToAllTCAtypes(
+        'pages',
+        '--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.palettes.editorial;editorial',
+        $doktypes,
+        'after:palette:metatags'
+    );
+
+    ExtensionManagementUtility::addToAllTCAtypes(
+        'pages',
+        'media',
+        $doktypes,
+        'before:tsconfig_includes'
+    );
 });
