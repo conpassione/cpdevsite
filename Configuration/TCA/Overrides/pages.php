767941a2f5195a02733b6dd07e2841ee85619786
Renzo Bauen <info@conpassione.ch>
2025-02-27 18:04:33 +0100

    stafflist translated, stafflist datasource selectbox reformated

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -5,7 +5,7 @@ defined('TYPO3') or die();
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
-call_user_func(static function(): void {
+call_user_func(static function (): void {
     // clear the default items for "layout" field to allow for consistent adding of additional items with addItems in
     // PageTSConfig (instead of a combination of altLabels and addItems)
     $GLOBALS['TCA']['pages']['columns']['layout']['config']['items'] = [];
@@ -13,11 +13,11 @@ call_user_func(static function(): void {
 
     // zusätzliche Icons für Folder-Seiten ... enthält Plugin Feld
     $GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = [
-        'label' => 'LLL:EXT:cpdevsite/Resources/Private/Language/locallang_db.xlf:pagetype.stafflist.label',
-        'icon' => 'tx-conpassione-stafflist',
-        'value'=>   'stafflist'
+        'label' => 'LLL:EXT:cpdevsite/Resources/Private/Language/locallang_db.xlf:pagetype.staff.label',
+        'icon' => 'tx-conpassione-staff',
+        'value' => 'staff'
     ];
-    $GLOBALS['TCA']['pages']['ctrl']['typeicon_classes']['contains-stafflist'] = 'tx-conpassione-stafflist';
+    $GLOBALS['TCA']['pages']['ctrl']['typeicon_classes']['contains-staff'] = 'tx-conpassione-staff';
 
 
     // Add group 'conpassione' to Page Types Selection
