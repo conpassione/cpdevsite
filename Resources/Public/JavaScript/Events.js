3caad7dcc8b80ee135086fca55145f7cba3efc15
Renzo Bauen <info@conpassione.ch>
2025-04-02 22:17:26 +0200

    pid of db-form in settings.yaml configurable

--- Resources/Public/JavaScript/Events.js
+++ Resources/Public/JavaScript/Events.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
   eventTitle = $('.h1--pagetitle').html();
   $('.form-event-title > input').attr('value', $.trim(eventTitle));
+
+  eventPid = $('.event').data("id");
+  $('.form-event-pid > input').attr('value', eventPid);
 });
